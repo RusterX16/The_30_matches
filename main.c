db4a84dd9cf592ea9d47b70a35b80a9c8aac2ecc
RusterX16 <EliottRuster@gmail.com>
2022-10-11 10:29:42 +0200

    Code Cleanup

--- main.c
+++ main.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void displayMatches(int matches);
+void breakLine();
+int chooseAmount();
+int generateTake();
+int checkOver(int matches, int turn);
+
 int main() {
     int over = 0;
     int turn = 1;
@@ -13,55 +19,83 @@ int main() {
         printf("--- Round %d ---\n", round);
 
         if (turn) {
-            printf("A vous de jouer !\n\n");
+            printf("A vous de jouer !");
+            breakLine();
             printf("Allumettes restantes : %d\n", matches);
 
-            for (int i = 0; i < matches; i++) {
-                printf("| ");
-            }
-            printf("\n\n");
+            displayMatches(matches);
+            breakLine();
 
-            int take;
+            int take = chooseAmount();
 
-            do {
-                printf("Combien d'allumettes voulez-vous retirer ?\n");
-                scanf("%d", &take);
+            printf("Vous retirez %d allumettes.", take);
+            breakLine();
 
-                if (take < 1 || take > 3) {
-                    printf("Tu n'as le droit que de retirer 1, 2 ou 3 allumettes !\n");
-                }
-            } while (take < 1 || take > 3);
-
-            printf("Vous retirez %d allumettes.\n\n", take);
             matches -= take;
-
-            if (matches <= 0) {
-                printf("Vous avez perdu contre le bot !\n");
-                over = 1;
-            }
+            over = checkOver(matches, take);
             turn = 0;
         } else {
-            printf("Au bot de jouer !\n\n");
+            printf("Au bot de jouer !");
+            breakLine();
             printf("Allumettes restantes : %d\n", matches);
 
-            for (int i = 0; i < matches; i++) {
-                printf("| ");
-            }
-            printf("\n\n");
+            displayMatches(matches);
+            breakLine();
 
-            int take = rand() % 3 + 1;
+            int take = generateTake();
 
-            printf("Le bot retire %d allumettes.\n\n", take);
+            printf("Le bot retire %d allumettes.", take);
+            breakLine();
+
             matches -= take;
-
-            if (matches <= 0) {
-                printf("Vous avez gagne la partie !\n");
-                over = 1;
-            }
+            over = checkOver(matches, take);
             turn = 1;
         }
         round++;
     }
+    return 0;
+}
 
+void displayMatches(int matches) {
+    for (int i = 0; i < matches; i++) {
+        printf("| ");
+    }
+    for (int i = 0; i < 30 - matches; i++) {
+        printf(". ");
+    }
+}
+
+void breakLine() {
+    printf("\n\n");
+}
+
+int chooseAmount() {
+    int take;
+
+    do {
+        printf("Combien d'allumettes voulez-vous retirer ?\n");
+        scanf("%d", &take);
+
+        if (take < 1 || take > 3) {
+            printf("Tu n'as le droit que de retirer 1, 2 ou 3 allumettes !\n");
+        }
+    } while (take < 1 || take > 3);
+
+    return take;
+}
+
+int generateTake() {
+    return rand() % 3 + 1;
+}
+
+int checkOver(int matches, int turn) {
+    if (matches <= 0) {
+        if (turn) {
+            printf("Vous avez gagne !\n");
+        } else {
+            printf("Vous avez perdu !\n");
+        }
+        return 1;
+    }
     return 0;
 }
